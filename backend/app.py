7c0b1289b9aa79a34ccc569c8ac92d8dbefa85bf
Aryan-Dev26 <227740686+Aryan-Dev26@users.noreply.github.com>
2025-09-18 12:09:58 +0530

    Basic CSS styling added with some minor Bug Fixes

--- backend/app.py
+++ backend/app.py
@@ -5,20 +5,19 @@ Author: Aryan Pravin Sahu
 
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='../frontend/templates', static_folder='..frontend/staic')
+app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
 
 @app.route('/')
 def home():
-    """Main Landing Page"""
+    """Main landing page"""
     return render_template('index.html')
 
-@app.route('/algoritms')
+@app.route('/algorithms')
 def algorithms():
     """Algorithm selection page"""
-    return render_template('algorithm.html')
+    return render_template('algorithms.html')
 
 if __name__ == '__main__':
-    print("Algorithm Visualizer Starting...")
+    print("Algorithm Visualizer starting...")
     print("Visit: http://localhost:5000")
     app.run(debug=True)
-
